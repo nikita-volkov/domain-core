110097b3333cdd6fd5283d887a367001e7c96a63
Nikita Volkov <nikita.y.volkov@mail.ru>
2023-12-09 15:40:57 +0300

    Modernize

--- library/DomainCore/Deriver.hs
+++ library/DomainCore/Deriver.hs
@@ -1,36 +1,32 @@
-{-|
-Toolkit for definition of instance derivers for Domain specs.
--}
+-- |
+-- Toolkit for definition of instance derivers for Domain specs.
 module DomainCore.Deriver
-(
-  -- * Deriver definitions
-  Deriver(..),
-  effectless,
-)
+  ( -- * Deriver definitions
+    Deriver (..),
+    effectless,
+  )
 where
 
-import DomainCore.Prelude
 import DomainCore.Model
-import qualified Language.Haskell.TH as TH (Q, Dec)
+import DomainCore.Prelude
+import qualified Language.Haskell.TH as TH (Dec, Q)
 
-
-{-|
-Specification of which instances to automatically derive for all the
-supported types in the model and how.
-
-You can combine derivers using Monoid and Semigroup.
--}
-newtype Deriver =
-  {-|
-  Function from the type declaration in this package\'s own AST
-  to a list of Template Haskell declarations in its quotation monad.
-  -}
-  Deriver (TypeDec -> TH.Q [TH.Dec])
-  deriving (Semigroup, Monoid)
+-- |
+-- Specification of which instances to automatically derive for all the
+-- supported types in the model and how.
+--
+-- You can combine derivers using Monoid and Semigroup.
+newtype Deriver
+  = -- |
+    --  Function from the type declaration in this package\'s own AST
+    --  to a list of Template Haskell declarations in its quotation monad.
+    Deriver (TypeDec -> TH.Q [TH.Dec])
+  deriving
+    (Semigroup, Monoid)
     via ((->) TypeDec (Ap TH.Q [TH.Dec]))
 
-{-|
-Lift a pure function, which doesn't require the context of 'TH.Q'.
--}
+-- |
+-- Lift a pure function, which doesn't require the context of 'TH.Q'.
+effectless :: (TypeDec -> [TH.Dec]) -> Deriver
 effectless f =
   Deriver (pure . f)
